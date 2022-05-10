0d976c8691e67cc7a3d1f11a7b63f2b9509b31fa
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-05-10 15:37:56 -0300

    Numeric Imput Field

---
+++ Assets/Script/DataInputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+    public Text txtUserName;
+    public void ShowGrettings()
+    {
+        Debug.Log("Hola " + txtUserName.text);
+    }
+
+    public void ShowDouble()
+    {
+        if (txtUserName.text != "")
+        {
+          Debug.Log(int.Parse(txtUserName.text) * 2);
+        }
+        else
+        {
+            Debug.Log("El campo no puede estar vacío");
+        }
+    }
+    
+
+}
